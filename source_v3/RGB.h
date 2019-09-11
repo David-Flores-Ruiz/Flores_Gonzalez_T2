45429a7189ceb0347e7331ffec247f81757327da
rgpacas8 <ie706818@iteso.mx>
2019-09-11 18:04:17 -0500

    RGB corregido

---
+++ source_v3/RGB.h
@@ -0,0 +1,36 @@
+/*
+ * RGB.h
+ *
+ *  Created on: 10 sep 2019
+ *      Author: pacas
+ */
+
+#ifndef RGB_H_
+#define RGB_H_
+
+#include "stdint.h"
+
+/*Constante que representa la habilitacion del clock GPIOA*/
+//#define SET_RED_ON 0x00400000;
+//#define SET_GREEN_ON 0x4000000;
+//#define SET_BLUE_ON 0x00200000;
+
+//#define SET_RED_OFF 0x00400000;
+//#define SET_GREEN_OFF 0x4000000;
+//#define SET_BLUE_OFF 0x00200000;
+
+
+
+typedef enum{RED_ON, /*!< Definition to select GPIO A */
+			 GREEN_ON, /*!< Definition to select GPIO B */
+			 BLUE_ON, /*!< Definition to select GPIO C */
+			} color_ON;
+typedef enum{RED_OFF, /*!< Definition to select GPIO A */
+			 GREEN_OFF, /*!< Definition to select GPIO B */
+			 BLUE_OFF, /*!< Definition to select GPIO C */
+			} color_OFF;
+
+			uint8_t encender_LED(color_ON color_encendido);
+			uint8_t apagar_LED(color_OFF color_apagar);
+
+#endif /* RGB_H_ */
